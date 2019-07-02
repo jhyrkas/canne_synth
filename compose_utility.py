1792a372cc9032110e63391748a38f61c72716be
Jeremy Hyrkas <hyrkas@10-16-1-140.dynapool.wireless.nyu.edu>
2019-07-02 12:08:15 -0400

    merging private changings with forked branch

---
+++ compose_utility.py
@@ -0,0 +1,111 @@
+import sys
+from canne import *
+import os
+import numpy as np
+import soundfile as sf
+import librosa
+
+class ComposeUtility :
+
+    def __init__(self):
+        mode = OperationMode(train=False,new_init=False,control=True,bias=True, zero_out_biases=True)
+        self.synth = ANNeSynth(mode)
+        self.synth.load_weights_into_memory()
+        self.out_size = 2049
+        self.len_window = 4096
+        self.hop_length = 1024
+        self.fs = 44100
+        self.prev_frame = np.zeros((1, self.out_size))
+
+    # generate note from middle layer
+    def make_note_osc(self, params, length) :
+        num_frames = self.get_num_frames(length)
+        mag_buf = np.zeros((self.out_size, num_frames))
+        if params.shape == (8,) or params.shape == (1, 8) :
+            params = params.reshape(1, 8)
+            for i in range(num_frames) :
+                mag_buf[:,i] = self.synth.generate_audio(params, bass_boost = True)
+        else :
+            # shape must be (n, 8)
+            assert(params.shape[1] == 8)
+            for i in range(num_frames) :
+                j = i
+                if j >= params.shape[0] :
+                    j = params.shape[0] - 1
+                act = params[j,:].reshape(1, 8)
+                mag_buf[:,i] = self.synth.generate_audio(act, bass_boost = True)
+
+        sig = do_rtpghi_gaussian_window(mag_buf, self.len_window, self.hop_length)
+        return sig
+
+    # generate note by full prediction, including feedback
+    # prev_frame will be for successive calls to predict audio, perhaps...
+    def predict_audio(self, audio, params = None, feedback_rate = 0, use_prev_frame = False) :
+        input_frames = self.get_input_frames(audio)
+        num_frames = input_frames.shape[1]
+        mag_buf = np.zeros(input_frames.shape)
+        feedback_frame = np.zeros((1, self.out_size))
+        if use_prev_frame :
+            # NOTE: copy here is probably unnecessary and the assignment at the end of this function
+            # would be unnecessary. however, this is probably preferable for debugging
+            # so no one has to worry about finding weird pass-by-reference bugs
+            feedback_frame = np.copy(self.prev_frame)
+        # could be argued that params should stay to their last value...
+        if params is None :
+            params = np.zeros(8)
+        if params.shape == (8,) or params.shape == (1, 8) :
+            params = params.reshape(8,)
+            for i in range(num_frames) :
+                in_frame = ((1.0 - feedback_rate) * input_frames[:,i].reshape(1, input_frames.shape[0])) + \
+                    (feedback_rate * feedback_frame)
+                mag_buf[:,i] = self.synth.generate_audio(
+                #    in_frame, bass_boost = True, full_mode = True, middle_weights = params)
+                    input_frames[:,i].reshape(1, input_frames.shape[0]),
+                    bass_boost = True, full_mode = True, middle_weights = params)
+                feedback_frame = np.copy(mag_buf[:,i].reshape(1, input_frames.shape[0]))
+                feedback_frame /= np.max(np.abs(feedback_frame))
+        else :
+            assert(params.shape[1] == 8)
+            for i in range(num_frames) :
+                j = i
+                if j >= params.shape[0] :
+                    j = params.shape[0] - 1
+                act = params[j,:].reshape(8,)
+                in_frame = ((1.0 - feedback_rate) * input_frames[:,i].reshape(1, input_frames.shape[0])) + \
+                    (feedback_rate * feedback_frame)
+                mag_buf[:,i] = self.synth.generate_audio(
+                    in_frame, bass_boost = True, full_mode = True, middle_weights = act)
+                feedback_frame = np.copy(mag_buf[:,i].reshape(1, input_frames.shape[0]))
+                feedback_frame /= np.max(np.max(feedback_frame))
+        # end predictions
+
+        self.prev_frame = feedback_frame
+        sig = do_rtpghi_gaussian_window(mag_buf, self.len_window, self.hop_length)
+        return sig
+       
+        
+
+    # mag frames for input audio, used for prediction
+    def get_input_frames(self, audio) :
+        # TODO might need to to some work here if the input size changes
+        frames = librosa.core.stft(
+            audio, n_fft = self.len_window, hop_length = self.hop_length)
+        frames = frames[0:(self.len_window//2 + 1),:]
+        mag_frames = np.abs(frames)
+        mag_max = np.max(mag_frames, axis=0)
+        return mag_frames / mag_max
+
+    # utility function
+    # TODO can i get this more accurate? not quite there
+    def get_num_frames(self, length) :
+        target_length = float(length * self.fs)
+        num_frames = int(np.ceil((target_length - self.out_size) / self.hop_length))
+        return num_frames
+
+    # TODO: add dreaming effect
+    def lstm_feedback(self, audio, length) :
+        pass
+
+    # TODO: pitch shifting algorithm here
+    def change_pitch(self, audio, shift) :
+        pass
--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-from PyQt4 import QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
 from canne import *
 import os
 import pygame
@@ -10,106 +12,107 @@ mode = OperationMode(train=False,new_init=False,control=True)
 synth = ANNeSynth(mode)
 
 class sliderGui(QWidget):
-	def __init__(self, parent = None):
-		super(sliderGui, self).__init__(parent)
-		layout = QVBoxLayout()
-		layout2 = QHBoxLayout()
-		
-		self.generateButton = QtGui.QPushButton('Save', self)
-		self.generateButton.clicked.connect(self.generate)
-		
-		self.playButton = QtGui.QPushButton('Pause',self)
-		self.playButton.clicked.connect(self.pause)
+        def __init__(self, parent = None):
+                super(sliderGui, self).__init__(parent)
+                layout = QVBoxLayout()
+                layout2 = QHBoxLayout()
+                
+                self.generateButton = QtWidgets.QPushButton('Save', self)
+                self.generateButton.clicked.connect(self.generate)
+                
+                self.playButton = QtWidgets.QPushButton('Pause',self)
+                self.playButton.clicked.connect(self.pause)
 
-		layout = QtGui.QVBoxLayout(self)
-		layout.addWidget(self.playButton)
-		layout.addWidget(self.generateButton)
-		layout.addLayout(layout2)
+                layout = QtWidgets.QVBoxLayout(self)
+                layout.addWidget(self.playButton)
+                layout.addWidget(self.generateButton)
+                layout.addLayout(layout2)
 
-		self.s1 = QSlider(Qt.Vertical)
-		self.s2 = QSlider(Qt.Vertical)
-		self.s3 = QSlider(Qt.Vertical)
-		self.s4 = QSlider(Qt.Vertical)
-		self.s5 = QSlider(Qt.Vertical)
-		self.s6 = QSlider(Qt.Vertical)
-		self.s7 = QSlider(Qt.Vertical)
-		self.s8 = QSlider(Qt.Vertical)
-		self.s9 = QSlider(Qt.Horizontal)
+                self.s1 = QSlider(Qt.Vertical)
+                self.s2 = QSlider(Qt.Vertical)
+                self.s3 = QSlider(Qt.Vertical)
+                self.s4 = QSlider(Qt.Vertical)
+                self.s5 = QSlider(Qt.Vertical)
+                self.s6 = QSlider(Qt.Vertical)
+                self.s7 = QSlider(Qt.Vertical)
+                self.s8 = QSlider(Qt.Vertical)
+                self.s9 = QSlider(Qt.Horizontal)
 
-		self.addSlider(self.s1,layout2)
-		self.addSlider(self.s2,layout2)
-		self.addSlider(self.s3,layout2)
-		self.addSlider(self.s4,layout2)
-		self.addSlider(self.s5,layout2)
-		self.addSlider(self.s6,layout2)
-		self.addSlider(self.s7,layout2)
-		self.addSlider(self.s8,layout2)
-		self.addSlider(self.s9,layout2)
-		self.s9.setMinimum(-30)
-		self.s9.setMaximum(30)
-		self.s9.setValue(0)
-		self.s9.setTickInterval(3)
+                self.addSlider(self.s1,layout2)
+                self.addSlider(self.s2,layout2)
+                self.addSlider(self.s3,layout2)
+                self.addSlider(self.s4,layout2)
+                self.addSlider(self.s5,layout2)
+                self.addSlider(self.s6,layout2)
+                self.addSlider(self.s7,layout2)
+                self.addSlider(self.s8,layout2)
+                self.addSlider(self.s9,layout2)
+                self.s9.setMinimum(-30)
+                self.s9.setMaximum(30)
+                self.s9.setValue(0)
+                self.s9.setTickInterval(3)
 
-		self.setLayout(layout)
-		self.setWindowTitle("CANNe")
+                self.setLayout(layout)
+                self.setWindowTitle("CANNe")
 
-	def addSlider(self,slider,layout):
-		slider.setMinimum(0)
-		slider.setMaximum(40)
-		slider.setValue(10)
-		slider.setTickPosition(QSlider.TicksBelow)
-		slider.setTickInterval(2)
-		layout.addWidget(slider)
-		slider.sliderReleased.connect(self.valuechange)		
+        def addSlider(self,slider,layout):
+                slider.setMinimum(0)
+                slider.setMaximum(40)
+                slider.setValue(10)
+                slider.setTickPosition(QSlider.TicksBelow)
+                slider.setTickInterval(2)
+                layout.addWidget(slider)
+                slider.sliderReleased.connect(self.valuechange)         
 
-	def valuechange(self):
-		tmp = np.zeros((1,9))
-		tmp[0,0] = self.s1.value()
-		tmp[0,1] = self.s2.value()
-		tmp[0,2] = self.s3.value()
-		tmp[0,3] = self.s4.value()
-		tmp[0,4] = self.s5.value()
-		tmp[0,5] = self.s6.value()
-		tmp[0,6] = self.s7.value()
-		tmp[0,7] = self.s8.value()
-		tmp /= 10.
-		tmp[0,8] = 2*self.s9.value()
-		synth.play_synth(tmp)
-		pygame.mixer.music.load('loop.wav')
-		pygame.mixer.music.play(-1)
+        def valuechange(self):
+                tmp = np.zeros((1,9))
+                tmp[0,0] = self.s1.value()
+                tmp[0,1] = self.s2.value()
+                tmp[0,2] = self.s3.value()
+                tmp[0,3] = self.s4.value()
+                tmp[0,4] = self.s5.value()
+                tmp[0,5] = self.s6.value()
+                tmp[0,6] = self.s7.value()
+                tmp[0,7] = self.s8.value()
+                tmp /= 10.
+                tmp[0,8] = 2*self.s9.value()
+                print(tmp)
+                synth.play_synth(tmp)
+                pygame.mixer.music.load('loop.wav')
+                pygame.mixer.music.play(-1)
 
 
-	def generate(self):
-		tmp = np.zeros((1,9))
-		tmp[0,0] = self.s1.value()
-		tmp[0,1] = self.s2.value()
-		tmp[0,2] = self.s3.value()
-		tmp[0,3] = self.s4.value()
-		tmp[0,4] = self.s5.value()
-		tmp[0,5] = self.s6.value()
-		tmp[0,6] = self.s7.value()
-		tmp[0,7] = self.s8.value()
-		tmp /= 10.
-		tmp[0,8] = self.s9.value()
-		text, ok = QInputDialog.getText(self, 'Save File', 'Enter filename:')
-		if ok:
-			filename_=str(text)
-			synth.execute(tmp,filename_)
+        def generate(self):
+                tmp = np.zeros((1,9))
+                tmp[0,0] = self.s1.value()
+                tmp[0,1] = self.s2.value()
+                tmp[0,2] = self.s3.value()
+                tmp[0,3] = self.s4.value()
+                tmp[0,4] = self.s5.value()
+                tmp[0,5] = self.s6.value()
+                tmp[0,6] = self.s7.value()
+                tmp[0,7] = self.s8.value()
+                tmp /= 10.
+                tmp[0,8] = self.s9.value()
+                text, ok = QInputDialog.getText(self, 'Save File', 'Enter filename:')
+                if ok:
+                        filename_=str(text)
+                        synth.execute(tmp,filename_)
 
-		
+                
 
-	def pause(self):
-		pygame.mixer.music.stop()
+        def pause(self):
+                pygame.mixer.music.stop()
 
 
 def main():
-	synth.load_weights_into_memory()
-	pygame.init()
-	pygame.mixer.init(channels=1)
-	app = QApplication(sys.argv)
-	ex = sliderGui()
-	ex.show()
-	sys.exit(app.exec_())
-	
+        synth.load_weights_into_memory()
+        pygame.init()
+        pygame.mixer.init(channels=1)
+        app = QApplication(sys.argv)
+        ex = sliderGui()
+        ex.show()
+        sys.exit(app.exec_())
+        
 if __name__ == '__main__':
-	main()
+        main()
