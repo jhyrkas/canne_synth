5d9153f499643e0980de1a4c8c3f2f2815214e7e
Jeremy Hyrkas <jeremy.hyrkas@gmail.com>
2019-07-25 15:07:11 -0400

    test script changes for running on hpc cluster

--- test_script.py
+++ test_script.py
@@ -1,15 +1,38 @@
 import numpy as np
 import dream_lstm
-import soundfile as sf
+#import soundfile as sf
+import wave
+from struct import pack, unpack
 
-#sig = np.sin(2*np.pi*440.0*np.array(range(88200))/44100)
-[sig, fs] = sf.read('minicomp.wav')
-sig_mono = np.mean(sig, axis=1)
-temp = dream_lstm.TimeDomainLSTM(sig_mono[:44100*30], 1024, 0.1, 0.05)
+#wf_read = wave.open( 'minicomp.wav', 'rb')
+#fs = wf_read.getframerate()
+#size = wf_read.getnframes()
+#tmp_sig = [[0] * size, [0] * size]
+#for i in range(size) :
+#    a,b = unpack('hh', wf_read.readframes(1))
+#    tmp_sig[0][i] = a
+#    tmp_sig[1][i] = b
+
+#sig = np.array(tmp_sig)
+#sig = sig / 2**15
+#sig_mono = np.mean(sig, axis=0)
+#wf_read.close()
+sig_mono = np.sin(2*np.pi*440.0*np.array(range(88200))/44100)
+#[sig, fs] = sf.read('minicomp.wav')
+#sig_mono = np.mean(sig, axis=1)
+temp = dream_lstm.TimeDomainLSTM(sig_mono, 1024, 0.1, 0.05)
 print('training!')
 temp.train_model(5)
 print('dreaming!')
 samples = temp.dream(temp.x[0,:], 44100*5)
 samples = samples / np.max(np.abs(samples))
-sf.write('dream.wav', samples, fs)
+
+wf_write = wave.open('dream.wav', 'w')
+wf_write.setnchannels(1)      # one channel (mono)
+wf_write.setsampwidth(2)      # two bytes per sample
+wf_write.setframerate(RATE)   # samples per second
+out_sig_tmp = (samples * (12**15)).tolist()
+wf_write.writeframes(pack('h' * len(out_sig_tmp), *out_sig_tmp))
+wf_write.close()
+#sf.write('dream.wav', samples, fs)
 print('done!')
