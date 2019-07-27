4aacf39bdb487732efd2b5a9b7f5db456dbaf398
Jeremy Hyrkas <jeremy.hyrkas@gmail.com>
2019-07-27 11:36:38 -0400

    some changes for stateful compatibility

--- dream_lstm.py
+++ dream_lstm.py
@@ -6,6 +6,7 @@ from tensorflow.keras.layers import LSTM,  Dense
 class TimeDomainLSTM :
     def __init__(self, input_audio, segment_length, input_dropout = 0.0, recurrent_dropout = 0.0) :
         self.seed_size = segment_length
+        self.batch_size = 100 # parameterize?
         self.create_training(input_audio, segment_length)
         self.create_model(segment_length, input_dropout, recurrent_dropout)
         
@@ -27,7 +28,7 @@ class TimeDomainLSTM :
         self.model.add(
             LSTM(segment_length, dropout = input_dropout, \
                  recurrent_dropout = recurrent_dropout, input_shape=(None,self.x.shape[1]),
-                 stateful = True)
+                 stateful = True, batch_input_shape = (self.batch_size, 1, self.x.shape[1]))
             )
         self.model.add(Dense(1)) # output shape of 1
         # dream uses squared error, but not sure when range is [-1 1]
@@ -35,20 +36,28 @@ class TimeDomainLSTM :
 
     def train_model(self, num_epochs) :
         data = self.x.reshape(self.x.shape[0], 1, self.x.shape[1])
+        # bit of a hack: truncate for stateful execution
+        mod = data.shape[0] % self.batch_size
         for i in range(num_epochs) :
             print('epoch #' + str(i+1))
-            self.model.fit(data, self.y, epochs=1, batch_size=1)
+            self.model.fit(data[0:data.shape[0] - mod, :, :], self.y[0:data.shape[0] - mod,:], 
+                    epochs=1, batch_size=self.batch_size, shuffle=False)
             self.reset_model()
 
-    def dream(self, seed, num_samples) :
+    def dream(self, num_samples) :
         self.reset_model()
-        seed = seed.reshape(1, 1, self.seed_size)
+        seed = self.x[0:self.batch_size, :]
+        seed = seed.reshape(self.batch_size, 1, self.seed_size)
         samples = np.zeros(num_samples)
         for i in range(num_samples) :
             pred = self.model.predict(seed)
-            samples[i] = pred
-            seed[0,0,0:-1] = seed[0, 0, 1:]
-            seed[0, 0, -1] = pred
+            #if i == 0 :
+            #    print(pred.shape)
+            #    print(pred)
+            samples[i] = pred[-1,0]
+            tmp = seed.flatten()
+            tmp = np.array(tmp.tolist()[1:] + [pred[-1,0]])
+            seed = tmp.reshape(self.batch_size, 1, self.seed_size)
 
         self.reset_model()
         return samples
