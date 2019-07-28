6e14be41ddeba9cc64a1c3d3c92b3d3629a9dece
Jeremy Hyrkas <jeremy.hyrkas@gmail.com>
2019-07-28 10:58:02 -0400

    keeping two version, one stateful and one not. testing for multiple LSTMs

--- dream_lstm_seq.py
+++ dream_lstm_seq.py
@@ -6,6 +6,7 @@ from tensorflow.keras.layers import LSTM,  Dense
 class TimeDomainLSTM :
     def __init__(self, input_audio, segment_length, input_dropout = 0.0, recurrent_dropout = 0.0) :
         self.seed_size = segment_length
+        self.batch_size = 100 # parameterize?
         self.create_training(input_audio, segment_length)
         self.create_model(segment_length, input_dropout, recurrent_dropout)
         
@@ -24,13 +25,10 @@ class TimeDomainLSTM :
 
     def create_model(self, segment_length, input_dropout, recurrent_dropout) :
         self.model = Sequential()
-        # trying with two LSTMs
         self.model.add(
             LSTM(segment_length, dropout = input_dropout, \
-                 recurrent_dropout = recurrent_dropout, input_shape=(None,self.x.shape[1]))
-            )
-        self.model.add(
-            LSTM(segment_length, dropout = input_dropout, recurrent_dropout = recurrent_dropout)
+                 recurrent_dropout = recurrent_dropout, input_shape=(None,self.x.shape[1]),
+                 stateful = True, batch_input_shape = (self.batch_size, 1, self.x.shape[1]))
             )
         self.model.add(Dense(1)) # output shape of 1
         # dream uses squared error, but not sure when range is [-1 1]
@@ -39,15 +37,32 @@ class TimeDomainLSTM :
 
     def train_model(self, num_epochs) :
         data = self.x.reshape(self.x.shape[0], 1, self.x.shape[1])
-        self.model.fit(self.y[0:data.shape[0] - mod,:], epochs=num_epochs)
+        # bit of a hack: truncate for stateful execution
+        mod = data.shape[0] % self.batch_size
+        for i in range(num_epochs) :
+            print('epoch #' + str(i+1))
+            self.model.fit(data[0:data.shape[0] - mod, :, :], self.y[0:data.shape[0] - mod,:], 
+                    epochs=1, batch_size=self.batch_size, shuffle=False)
+            self.reset_model()
 
-    def dream(self, seed, num_samples) :
-        seed = seed.reshape(1, 1, self.seed_size)
+    def dream(self, num_samples) :
+        self.reset_model()
+        seed = self.x[0:self.batch_size, :]
+        seed = seed.reshape(self.batch_size, 1, self.seed_size)
         samples = np.zeros(num_samples)
         for i in range(num_samples) :
             pred = self.model.predict(seed)
-            samples[i] = pred
-            seed[0,0,0:-1] = seed[0, 0, 1:]
-            seed[0, 0, -1] = pred
+            #if i == 0 :
+            #    print(pred.shape)
+            #    print(pred)
+            samples[i] = pred[-1,0]
+            tmp = seed.flatten()
+            tmp = np.array(tmp.tolist()[1:] + [pred[-1,0]])
+            seed = tmp.reshape(self.batch_size, 1, self.seed_size)
 
+        self.reset_model()
         return samples
+
+    # use for stateful LSTMS, either before training or before dreaming
+    def reset_model(self) :
+        self.model.reset_states()
---
+++ test_stateful.py
@@ -0,0 +1,56 @@
+import numpy as np
+import dream_lstm
+#import soundfile as sf
+import wave
+from struct import pack, unpack
+
+# STEREO TEST
+#wf_read = wave.open( 'minicomp.wav', 'rb')
+#fs = wf_read.getframerate()
+#size = wf_read.getnframes()
+#tmp_sig = [[0] * size, [0] * size]
+#for i in range(size) :
+#    a,b = unpack('hh', wf_read.readframes(1))
+#    tmp_sig[0][i] = a
+#    tmp_sig[1][i] = b
+
+#sig = np.array(tmp_sig)
+#sig = sig / 2**15
+#sig_mono = np.mean(sig, axis=0)
+#wf_read.close()
+
+# MONO TEST
+#wf_read = wave.open( 'second_input_16bit.wav', 'rb')
+#fs = wf_read.getframerate()
+#size = wf_read.getnframes()
+#tmp_sig = unpack('h'*size, wf_read.readframes(size))
+
+#sig = np.array(tmp_sig)
+#sig_mono = sig / 2**15
+#wf_read.close()
+
+# SIN WAVE TEST
+sig_mono = np.sin(2*np.pi*440.0*np.array(range(44100*15))/44100)
+
+#[sig, fs] = sf.read('minicomp.wav')
+#sig_mono = np.mean(sig, axis=1)
+
+print(sig_mono.shape)
+
+temp = dream_lstm.TimeDomainLSTM(sig_mono[0:44100*15], 1024, 0.1, 0.05)
+print('training!')
+temp.train_model(50)
+print('dreaming!')
+#samples = temp.dream(temp.x[0,:], 44100*10)
+samples = temp.dream(44100*10)
+samples = samples / np.max(np.abs(samples))
+
+wf_write = wave.open('dream.wav', 'w')
+wf_write.setnchannels(1)      # one channel (mono)
+wf_write.setsampwidth(2)      # two bytes per sample
+wf_write.setframerate(44100)   # samples per second
+out_sig_tmp = np.clip(samples * (2**15), -(2**15), 2**15 - 1).astype(int).tolist()
+wf_write.writeframes(pack('h' * len(out_sig_tmp), *out_sig_tmp))
+wf_write.close()
+#sf.write('dream.wav', samples, fs)
+print('done!')
