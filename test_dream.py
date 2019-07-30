2c41be3ad29bca5b684f0bfb09fc4466e569d5b4
Jeremy Hyrkas <jeremy.hyrkas@gmail.com>
2019-07-30 10:26:37 -0400

    testing new dreaming algorithm on guitar

--- test_dream.py
+++ test_dream.py
@@ -6,35 +6,35 @@ from struct import pack, unpack
 from random import shuffle
 
 # STEREO TEST
-wf_read = wave.open( 'minicomp.wav', 'rb')
-fs = wf_read.getframerate()
-size = wf_read.getnframes()
-tmp_sig = [[0] * size, [0] * size]
-for i in range(size) :
-    a,b = unpack('hh', wf_read.readframes(1))
-    tmp_sig[0][i] = a
-    tmp_sig[1][i] = b
-
-sig = np.array(tmp_sig)
-sig = sig / 2**15
-sig_mono = np.mean(sig, axis=0)
-wf_read.close()
-
-# MONO TEST
-#wf_read = wave.open( 'second_input_16bit.wav', 'rb')
+#wf_read = wave.open( 'minicomp.wav', 'rb')
 #fs = wf_read.getframerate()
 #size = wf_read.getnframes()
-#tmp_sig = unpack('h'*size, wf_read.readframes(size))
+#tmp_sig = [[0] * size, [0] * size]
+#for i in range(size) :
+#    a,b = unpack('hh', wf_read.readframes(1))
+#    tmp_sig[0][i] = a
+#    tmp_sig[1][i] = b
 
 #sig = np.array(tmp_sig)
-#sig_mono = sig / 2**15
+#sig = sig / 2**15
+#sig_mono = np.mean(sig, axis=0)
 #wf_read.close()
+
+# MONO TEST
+wf_read = wave.open( 'second_input_16bit.wav', 'rb')
+fs = wf_read.getframerate()
+size = wf_read.getnframes()
+tmp_sig = unpack('h'*size, wf_read.readframes(size))
+
+sig = np.array(tmp_sig)
+sig_mono = sig / 2**15
+wf_read.close()
 
 # SIN WAVE TEST
 #sig_mono = np.sin(2*np.pi*440.0*np.array(range(44100*15))/44100)
 
-#[sig, fs] = sf.read('minicomp.wav')
-#sig_mono = np.mean(sig, axis=1)
+[sig, fs] = sf.read('minicomp.wav')
+sig_mono = np.mean(sig, axis=1)
 
 print(sig_mono.shape)
 
