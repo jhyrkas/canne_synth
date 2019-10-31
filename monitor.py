e6734a71834433c9a38276564f9bc0048b50b15a
Jeremy Hyrkas <hyrkas@Jeremys-iMac.home>
2019-10-31 14:23:16 -0400

    adding monitoring file for use with comp2.py (and generally monitoring anything that writes to soundflower)

---
+++ monitor.py
@@ -0,0 +1,17 @@
+import sounddevice as sd
+
+devices = sd.query_devices()
+indevice = 0
+outdevice = 0
+for i in range(len(devices)) :
+    if devices[i]['name'] == 'Soundflower (2ch)' :
+        indevice = i
+    if devices[i]['name'] == 'Built-in Output' :
+            outdevice = i
+
+instream = sd.InputStream(samplerate = 44100, device = indevice)
+outstream = sd.OutputStream(samplerate = 44100, device = outdevice)
+instream.start()
+outstream.start()
+while True :
+    outstream.write(instream.read(1024)[0])
