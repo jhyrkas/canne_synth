71c8cf8a7e2ae8065c5c294ca559bb2e83dcc269
Jeremy Hyrkas <hyrkas@Jeremys-iMac.home>
2019-12-02 19:36:52 -0500

    pitch shifting for algo music class

--- algomusic_comp.py
+++ algomusic_comp.py
@@ -5,8 +5,10 @@ import os
 import time
 
 def parse_params(params_raw) :
-    lengths = params_raw[:,0]
-    params_raw = params_raw[:,1:]
+    # first line is a dummy line to get the pitch information
+    pitch_shift = params_raw[0,-1]
+    lengths = params_raw[1:,0]
+    params_raw = params_raw[1:,1:]
     params = np.zeros((0,8))
     audio_len = 0.0
     for i in range(1, params_raw.shape[0]) :
@@ -27,7 +29,7 @@ def parse_params(params_raw) :
         params = np.append(params, temp, axis=0)
         audio_len += length
 
-    return params, audio_len
+    return params, audio_len, pitch_shift
 
 # TODO perhaps i can parameterize this
 basepath = '/Users/hyrkas/eclipse-workspace/AlgoMusicHW3/scratch/' 
@@ -39,9 +41,9 @@ arch = sa.Architecture('root', np.zeros(8) + 1.0)
 while True :
     if os.path.exists(param_path) :
         params_raw = np.genfromtxt(param_path, delimiter=',')
-        params, audio_len = parse_params(params_raw)
+        params, audio_len, pitch_shift = parse_params(params_raw)
         arch.update_params('root', params)
-        audio = arch.generate_audio(audio_len)[0]
+        audio = arch.generate_audio(audio_len, pitch_shift)[0]
         sf.write(wav_path, audio, 44100)
         os.remove(param_path)
     time.sleep(0.1)
