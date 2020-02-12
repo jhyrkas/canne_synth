98a1e5f2c504c6bcd221a1ccb13cfe58ee03fe95
Jeremy Hyrkas <hyrkas@10-17-115-170.dynapool.wireless.nyu.edu>
2020-02-12 15:38:21 -0500

    prettied up the graph

--- timing_analysis.R
+++ timing_analysis.R
@@ -4,5 +4,15 @@ df <- read.csv('analysis_files/timing.csv')
 
 ggplot(data=df, aes(x=num_seconds, y=average_time, group=Method)) + 
   geom_line(aes(color=Method)) + geom_point(aes(shape=Method)) +
+  xlab('Seconds of audio') + ylab('Generation time') +
   ggtitle('Average generation time by method') +
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5), 
+        panel.background = element_rect(fill = 'white', colour = 'white'),
+        panel.grid.major = element_line(size = 0.5, linetype = 'solid', colour = "grey"),
+        panel.grid.minor = element_line(size = 0.25, linetype = 'solid', colour = "grey")) +
+  scale_colour_discrete(name="Generational Method",
+                      breaks=c("network_mod_changing", "network_mod_static", "vanilla_changing", "vanilla_static"),
+                      labels=c("Network Mod. Variable Bias", "Network Mod. Static Bias", "Decoder Variable Encoding", "Decoder Static Encoding")) +
+  scale_shape_discrete(name="Generational Method",
+                        breaks=c("network_mod_changing", "network_mod_static", "vanilla_changing", "vanilla_static"),
+                        labels=c("Network Mod. Variable Bias", "Network Mod. Static Bias", "Decoder Variable Encoding", "Decoder Static Encoding"))
